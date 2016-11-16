01068272580011c62496aa0fd319cc52bb409f27
SeokJun Hong <hong921122@gmail.com>
2016-11-17 01:20:51 +0900

    string format

--- js/index.js
+++ js/index.js
@@ -1,5 +1,17 @@
 'use strict';
 
+// First, checks if it isn't implemented yet.
+if (!String.prototype.format) {
+    String.prototype.format = function() {
+        var args = arguments;
+        return this.replace(/{(\d+)}/g, function(match, number) {
+            return typeof args[number] != 'undefined'
+            ? args[number]
+            : match;
+        });
+    };
+}
+
 window.addEventListener("load", function () {
     var important_viewpoints = [],
         viewpoint_index = 0,
